3fe9b3743eb83ded964f9a33f829081a3a8abed7
meister.kay <ppedv@WIN10-BASE>
2018-04-19 15:24:58 +0200

    Konsumierenden Client hinzugefügt. => CORS-Problematik, da Client und Service auf unterschiedlichen Ports laufen.

---
+++ 20180419_WebApiSchulung/Api.AdventureWorks2012.ProductmanagementClient/Scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $('.products-container').html('Hello API');
+
+    $.ajax({
+        url: "http://localhost:3987/api/Product",
+        method: "GET"
+    }).done(function (data) {
+        console.log(data); // hier haben wir jetzt eine Fehlermeldung bzgl. CORS-Problematik im Log stehen.
+    });
+});
