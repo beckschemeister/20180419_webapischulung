05ac12bd798349fb10c5fcefd97b6bf4cf997ffb
meister.kay <ppedv@WIN10-BASE>
2018-04-19 11:11:09 +0200

    Verwendung eines ViewModels zur Datenanlage

--- 20180419_WebApiSchulung/Api.AdventureWorks2012.Productmanagement/Controllers/ProductController.cs
+++ 20180419_WebApiSchulung/Api.AdventureWorks2012.Productmanagement/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Api.AdventureWorks2012.Productmanagement.Models;
+using Api.AdventureWorks2012.Productmanagement.ViewModels;
 
 namespace Api.AdventureWorks2012.Productmanagement.Controllers
 {
@@ -60,9 +61,30 @@ namespace Api.AdventureWorks2012.Productmanagement.Controllers
 
         // POST api/Product
         [HttpPost] // <- damit der Methodenname mit Create und nicht mit POST beginnen kann. (Swagger kapiert das dankenswerterweise so).
-        public IHttpActionResult CreateProduct(Product product)
+        public IHttpActionResult CreateProduct(ProductViewModel productViewModel)
         {
-            return Created(new Uri(Request.RequestUri + "/" + product.ProductID), product);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var product = new Product
+            {
+                Name = productViewModel.Name,
+                ProductNumber = productViewModel.ProductNumber,
+
+                // Diese Felder braucht die Datenbank... das muss umgangen werden!
+                ModifiedDate = DateTime.UtcNow,
+                SellStartDate = DateTime.UtcNow,
+                SafetyStockLevel = 12
+            };
+            _productDbContext.Product.Add(product);
+            _productDbContext.SaveChanges();
+
+            productViewModel.ProductID = product.ProductID;
+            
+
+            return Created(new Uri(Request.RequestUri + "/" + productViewModel.ProductID), productViewModel);
         }
     }
 }
---
+++ 20180419_WebApiSchulung/Api.AdventureWorks2012.Productmanagement/ViewModels/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.AdventureWorks2012.Productmanagement.ViewModels
+{
+    /// <summary>
+    /// Minimalanforderung zum Erstellen eines Products.
+    /// </summary>
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string ProductNumber { get; set; }
+    }
+}
