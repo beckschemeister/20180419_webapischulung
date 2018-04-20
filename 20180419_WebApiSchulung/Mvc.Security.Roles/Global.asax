04a8c47b913e81e1df4b879195c30a9402a5d9e8
meister.kay <ppedv@WIN10-BASE>
2018-04-20 15:48:54 +0200

    Neues MVC-Projekt inkl. Web Api für Rollenmodell in Security angelegt

---
+++ 20180419_WebApiSchulung/Mvc.Security.Roles/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc.Security.Roles.MvcApplication" Language="C#" %>
---
+++ 20180419_WebApiSchulung/Mvc.Security.Roles/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc.Security.Roles.Startup))]
+namespace Mvc.Security.Roles
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
