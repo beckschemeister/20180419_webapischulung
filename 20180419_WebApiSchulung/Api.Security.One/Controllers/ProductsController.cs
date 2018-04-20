cdc9cf7e87fc2e27a283a21fcba006a4eed948ed
meister.kay <ppedv@WIN10-BASE>
2018-04-20 10:55:33 +0200

    Zu sichernden Service aufgesetzt

---
+++ 20180419_WebApiSchulung/Api.Security.One/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Api.Security.One.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IHttpActionResult GetAllProducts()
+        {
+            return Ok("Hier die Liste der Produkte");
+        }
+    }
+}
