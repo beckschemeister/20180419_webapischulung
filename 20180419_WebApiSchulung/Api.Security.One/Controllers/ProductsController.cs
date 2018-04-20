ac37ed6b58ee56090edd4ca54a961a0783827e5f
meister.kay <ppedv@WIN10-BASE>
2018-04-20 11:01:05 +0200

    UserSecret abfragen

--- 20180419_WebApiSchulung/Api.Security.One/Controllers/ProductsController.cs
+++ 20180419_WebApiSchulung/Api.Security.One/Controllers/ProductsController.cs
@@ -1,11 +1,22 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
 
 namespace Api.Security.One.Controllers
 {
     public class ProductsController : ApiController
     {
+        // Verwendung von User-Secret
+        private const string SECRET_KEY = "top-secret";
+        private const string Password = "123";
+        
         public IHttpActionResult GetAllProducts()
         {
+            if (!Request.Headers.Contains(SECRET_KEY) ||
+                !Request.Headers.GetValues(SECRET_KEY).First().Equals(Password))
+            {
+                return Unauthorized();
+            }
+
             return Ok("Hier die Liste der Produkte");
         }
     }
