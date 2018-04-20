2a7ea5d01f17e4f5ccc7c9499da22c6c7c704ee8
meister.kay <ppedv@WIN10-BASE>
2018-04-20 11:28:20 +0200

    Nugetpakete: Microsoft.Owin, Microsoft.Owin.Host.SystemWeb, Microsoft.AspNet.Identity.Owin, Microsoft.Owin.Security.OAuth. OwinStartup-Klasse hinzufügen und ausgestalten.

--- 20180419_WebApiSchulung/Api.Security.One/Controllers/ProductsController.cs
+++ 20180419_WebApiSchulung/Api.Security.One/Controllers/ProductsController.cs
@@ -3,19 +3,21 @@ using System.Web.Http;
 
 namespace Api.Security.One.Controllers
 {
+    [Authorize] // Authorisierung für den gesamten Controller notwendig
     public class ProductsController : ApiController
     {
         // Verwendung von User-Secret
-        private const string SECRET_KEY = "top-secret";
-        private const string Password = "123";
+        //private const string SECRET_KEY = "top-secret";
+        //private const string Password = "123";
         
         public IHttpActionResult GetAllProducts()
         {
-            if (!Request.Headers.Contains(SECRET_KEY) ||
-                !Request.Headers.GetValues(SECRET_KEY).First().Equals(Password))
-            {
-                return Unauthorized();
-            }
+            // Verwendung von User-Secret
+            //if (!Request.Headers.Contains(SECRET_KEY) ||
+            //    !Request.Headers.GetValues(SECRET_KEY).First().Equals(Password))
+            //{
+            //    return Unauthorized();
+            //}
 
             return Ok("Hier die Liste der Produkte");
         }
---
+++ 20180419_WebApiSchulung/Api.Security.One/Startup.Owin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(Api.Security.One.Startup))]
+
+namespace Api.Security.One
+{
+    public class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            // Weitere Informationen zum Konfigurieren Ihrer Anwendung finden Sie unter https://go.microsoft.com/fwlink/?LinkID=316888.
+            OAuthOptions = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new PathString("/token"), // hier gibt es den Token
+                Provider = new OAuthAuthorizationServerProvider()
+                {
+                    OnValidateClientAuthentication = async (context) =>
+                    {
+                        context.Validated();
+                    },
+                    OnGrantResourceOwnerCredentials = async (context) =>
+                    {
+                        // hier würde man die tatsächliche Quelle der User anbinden!
+                        if (context.UserName == "login@mail.com" && context.Password == "123")
+                        {
+                            ClaimsIdentity oAuthIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+                            context.Validated(oAuthIdentity);
+                        }
+                    }
+                },
+                AllowInsecureHttp = true,
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1)
+            };
+
+            app.UseOAuthBearerTokens(OAuthOptions);
+        }
+    }
+}
